ab69fa0648ad242f069e97f80b8e416ab3cc75ad
Martin Šíl <martin.sil1337@gmail.com>
2023-04-15 16:46:21 +0200

    API route pro ověření předplacení Discord botem

---
+++ web/pages/api/discord-verification.js
@@ -0,0 +1,33 @@
+import { getServiceSupabase } from '@/utils/supabase';
+
+const handler = async (req, res) => {
+	if (req.method === 'POST') {
+		const { userid } = req.body;
+
+		if (!userid) {
+			return res.status(400).json({ error: 'You did not send the user' });
+		}
+
+		const supabase = getServiceSupabase();
+
+		const { data: profile } = await supabase
+			.from('profile')
+			.select('subscribed')
+			.eq('discord_id', userid)
+         .single();
+         console.log(profile);
+		if (profile) {
+			return res
+				.status(200)
+				.json({ exist: true, subscribed: profile.subscribed });
+		} else {
+			return res.status(200).json({ exist: false, subscribed: null });
+		}
+	} else {
+		return res.status(405).json({
+			error: 'Method not allowed',
+		});
+	}
+};
+
+export default handler;
